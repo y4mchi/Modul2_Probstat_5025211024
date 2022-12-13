15f32af51ac028a902e275d0be41b9d5f5abaf47
y4mchi <100952724+y4mchi@users.noreply.github.com>
2022-12-13 18:49:19 +0700

    Create Modul2.r

---
+++ Modul2.r
@@ -0,0 +1,13 @@
+#soal 1
+
+#a
+x<- c(78, 75, 67, 77, 70, 72, 78, 74, 77)
+y<- c(100, 95, 70, 90, 90, 90, 89, 90, 100)
+
+#pengecekan data 
+dt <- data.frame(x,y)
+dt
+
+#Mencari Selisih 
+diff <- y-x
+diff
